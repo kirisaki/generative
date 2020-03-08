83b8dae0655225dcf2e71eab8672ac0937948f58
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-08 21:24:42 +0900

    feat: Euclid

--- src/components/Euclid.tsx
+++ src/components/Euclid.tsx
@@ -15,12 +15,37 @@ export const Euclid: React.FC = () => {
   const [state, setState] = useState(null)
   const [rects, setRects] = useState<Rect>([])
   useEffect(() => {
-    setRects([{x: 10, y:10, w:100, h:50}])
+    const scale = 50
+    const numA = 10 * scale
+    const numB = 6 * scale
+    let w = numB
+    let x = 0
+    let y = 0
+    let i =0
+    let results = []
+
+    while(w > 0){
+      i++
+      if(i % 2 == 1){
+        while(x + w <= numA){
+          results.push({x, y, w, h: w})
+          x += w
+        }
+        w = numA - x
+      }else{
+        while(y + w <= numB){
+          results.push({x, y, w, h: w})
+          y += w
+        }
+        w = numB - y
+      }
+    }
+    setRects(results)
   },[])
   return(
     <SvgCanvas callback={setState}>
       {rects.map((r, i) =>
-        <rect key = {i} x={r.x} y={r.y} width={r.w} height={r.h} stroke="black" strokeWidth="1" fill="none" />
+        <rect key = {i} x={r.x} y={r.y} width={r.w} height={r.h} stroke="#eee" strokeWidth="1" fill="none" />
       )}
     </SvgCanvas>
   )
